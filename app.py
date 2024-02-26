0b6361b2c6634043550ac3b15e72228ee5cf1ffd
ya-aqob <157934932+ya-aqob@users.noreply.github.com>
2024-02-26 17:36:07 -0500

    jwq

--- app.py
+++ app.py
@@ -1,34 +1,28 @@
 from flask import Flask, request
 import os
+import csv
 import psycopg2
 from dotenv import load_dotenv
 from dotenv import find_dotenv
 import hashlib, time
 import random
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
+key_path = '"C:\Users\jacob\recyclequest-abd56bc8581a.json"'
+sheet_id = '1H1-5p2iVq1dg0X31dbopoEbIa_gALT8je-Rom1XfIYM'
+creds = None
+if os.path.exist(key_path):
+    creds = service_account.Credentials.from_service_account_file(key_path, scopes=['https://www.googleapis.com/auth/spreadsheets'])
 
-#notes and to-do list
-#leaderboard sorting
+service = build('sheets', 'v4', credentials = creds)
 
-# path = placeholder sets path to .env file
-load_dotenv(verbose=True) # loads .env file
-url = os.getenv("DATABASE_URL") # gets url from .env file
 app = Flask(__name__) #starts flask app
-connection = psycopg2.connect(url) #creates database connection
 
 if __name__ == '__main__':
     app.run(debug=False)
 
-#constant group
-CREATE_USER_TABLE = ("CREATE TABLE IF NOT EXISTS userinfo (username TEXT, userid BIGINT, password TEXT, salt TEXT);") # creates new user table
-CREATE_LEADERBOARD_TABLE = ("CREATE TABLE IF NOT EXISTS leaderboard (id INT, points INT);") # creates new leaderboard table
-INSERT_USER_INFORMATION = ("INSERT INTO userinfo (username, userid, password, salt) VALUES (%s, %L, %s, %s);") # creates new user in user table
-INSERT_USER_LEADERBOARD = ("INSERT INTO leaderboard (id, points) VALUES (%L, %L)") # creates new user in leaderboard table
-UPDATE_NEW_POINTS = "UPDATE leaderboard SET points = (%L) WHERE id = (%L); " # adds points for given user
-FIND_USER_ID = "SELECT userid from userinfo WHERE username = (%s)" # grabs userid based on what username is
-SELECT_PASSWORD_VERIFICATION = "SELECT password, salt from userinfo WHERE userid = (%L)" # grabs saved hashed password and salt
-recycleConstant = 4 # points per item recycled
-disposalConstant = 3 # points per item disposed
 
 # creates user table and leaderboard table if none exists, adds new user with username and theoretically unique hashed userID and password, initializes points to zero for given user
 @app.post("/api/users")
@@ -41,13 +35,6 @@ def create_user():
     unhashedPass = (unhashedPass+salt)
     unhashedPass = unhashedPass.encode('ascii', 'utf-8')
     hashedPass = hashlib.sha256(bytes(unhashedPass)).hexdigest()
-    with connection:
-        with connection.cursor() as cursor:
-            cursor.execute(CREATE_USER_TABLE)
-            cursor.execute(INSERT_USER_INFORMATION, (userName, userID, hashedPass, salt))
-            cursor.execute(CREATE_LEADERBOARD_TABLE)
-            cursor.execute(INSERT_USER_LEADERBOARD, (userID, 0))
-    return {"userID": f"User {userName} created successfully.", "points": f"User {userName} has the ID {userID}."}
 
 # adds new points for a given user based on recycled items from client, the execute may or may not work
 @app.post("/api/leaderboard")
@@ -57,14 +44,7 @@ def update_leaderboard():
     itemsRecycled = data["recycledItems"]
     itemsDisposed = data["disposedItems"]
     newPoints = recycleConstant * itemsRecycled + disposalConstant * itemsDisposed
-    with connection:
-        with connection.cursor() as cursor:
-            cursor.execute(FIND_USER_ID, (userName))
-            userID = cursor.fetchall()
-            for row in userID:
-                userID = row[0]
-            cursor.execute(UPDATE_NEW_POINTS, (userID, newPoints))
-    return {"User": f"{userName} has earned "}
+
 
 # tests password attempt against hashed password and stored salt
 @app.post("/api/login")
@@ -72,36 +52,10 @@ def login_verified():
     data = request.get_json()
     userName = data["userName"]
     passAttempt = data["passwordAttempt"]
-    with connection:
-        with connection.cursor() as cursor:
-            cursor.execute(FIND_USER_ID, (userName))
-            userID = cursor.fetchall()
-            for row in userID:
-                userID = row[0]
-            cursor.execute(SELECT_PASSWORD_VERIFICATION, (userID))
-            passwordInfo = cursor.fetchall()
-            for row in passwordInfo:
-                hashedPassword = row[0]
-                salt = row[1]
     hashPassAttempt = passAttempt + salt
     hashPassAttempt = hashlib.sha256(bytes(hashPassAttempt))
-    if hashPassAttempt == hashedPassword:
-        return {"verificationStatus": f"{userName} has successfully been verified."}
-    else:
-        return {"verificationStatus": f"User not successfully verified."}
 
 
 @app.get("/api/leaderboard/<string:userName>")
-def get_user_points(userName):
-    with connection:
-        with connection.cursor() as cursor:
-            cursor.execute(FIND_USER_ID, (userName))
-            userID = cursor.fetchall()
-            for row in userID:
-                userID = row[0]
-            cursor.execute(FIND_USER_ID, (userName))
-            points = cursor.fetchall()
-            for row in points:
-                userPoints = row[0]
-    return {"pointsStatus": f"{userName} has {userPoints} points."}
+def get_user_points():
 
